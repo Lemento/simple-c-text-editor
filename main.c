fd3959693be08bd936a76acb4af6d089e3b96bd0
Dagai <>
2024-03-13 22:13:22 -0400

    text editor program now creates and refreshes a screen with a column of tildes on the side. Provides cursor movement using the arrow keys as well as the PAGE_UP and PAGE_DOWN keys to jump from the top the bottom of the window. Doesn't use any other kind of input or allow writing text yet.

--- main.c
+++ main.c
@@ -2,57 +2,294 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 void enableRawMode(void);
-void deathToMachine (char const* err_message);
+void ted_init(void);
+void ted_refreshScreen(void);
+void ted_processKeypress(void);
+
+#define KILO_VERSION "0.0.1"
+
+#define CTRL_KEY(K) ((K) & 0x1F)
+
+enum ted_Key
+{
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
+};
+
 int main(void)
 {
 	enableRawMode();
+	ted_init();
 
 	//  Read 1 byte of user input
 	//  terminal will not output user input and will exit on 'q' keypress
-	printf("Press 'q' to exit\r\n");
 	while (1)
 	{
-	  char c = '\0';
-	  if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-	  { deathToMachine("read"); }
-	  // print every input characters ascii value
-	  if (iscntrl(c))
-	  { printf("%d\r\n", c); }
-	  else{ printf("%d ('%c')\r\n", c, c); }
-
-	  if (c == 'q') break;
+	  ted_refreshScreen();
+	  ted_processKeypress();
 	}
 
 	return 0;
 }
 
-struct termios orig_termios;
+//  Returns size of current terminal in columns and rows
+int getWindowSize (int* rows, int* cols)
+{
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+	{ return -1; }
+	else
+	{
+	  *cols = ws.ws_col;
+	  *rows = ws.ws_row;
+	  return 0;
+	}
+}
 
 void deathToMachine (char const* err_message)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
 	perror(err_message);
 	exit(1);
 }
 
+//  terminal screen buffer
+struct abuf
+{
+	char* b;
+	int len;
+};
+#define ABUF_INIT {NULL, 0}
+
+void ab_append (struct abuf* ab, char const* s, int len)
+{
+	char*const new = realloc(ab->b, ab->len+len);
+
+	if (new == NULL){ return; }
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+}
+
+void ab_free (struct abuf* ab)
+{ free(ab->b); }
+
+struct ted_Config
+{
+	int cx, cy;
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+struct ted_Config E;
+
+void ted_init(void)
+{
+	E.cx = 0;
+	E.cy = 0;
+	if (getWindowSize (&E.screenrows, &E.screencols) == -1)
+	{ deathToMachine("getWindowSize"); }
+}
+
+int ted_readKey(void)
+{
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+	  if (nread == -1 && errno != EAGAIN)
+	  { deathToMachine("read"); }
+	}
+
+	if (c != '\x1b'){ return c; }
+	
+	char seq[3];
+
+	if (read(STDIN_FILENO, &seq[0], 1) != 1){ return '\x1b'; }
+	if (read(STDIN_FILENO, &seq[1], 1) != 1){ return '\x1b'; }
+
+	if (seq[0] == '[')
+	{
+	  if (seq[1] >= '0' && seq[1] <= '9')
+	  {
+	    if (read(STDIN_FILENO, &seq[2], 1) != 1){ return '\x1b'; }
+	    if (seq[2] == '~')
+	    {
+	      switch (seq[1])
+	      {
+	        case '1': return HOME_KEY;
+		case '3': return DEL_KEY;
+	        case '4': return END_KEY;
+	        case '5': return PAGE_UP;
+	        case '6': return PAGE_DOWN;
+	        case '7': return HOME_KEY;
+	        case '8': return END_KEY;
+	      }
+	    }
+	  } else {
+	    switch (seq[1])
+	    {
+	      case 'A': return ARROW_UP;
+	      case 'B': return ARROW_DOWN;
+	      case 'C': return ARROW_RIGHT;
+	      case 'D': return ARROW_LEFT;
+	      case 'H': return HOME_KEY;
+	      case 'F': return END_KEY;
+	    }
+	  }
+
+	} else if (seq[0] == 'O') {
+	  switch (seq[1])
+	  {
+	    case 'H': return HOME_KEY;
+	    case 'F': return END_KEY;
+	  }
+	}
+
+	return '\x1b';
+}
+
+void ted_moveCursor (int key)
+{
+	switch (key)
+	{
+	  case ARROW_LEFT:
+	    if (E.cx != 0)
+	    { E.cx--; }
+	    break;
+	  case ARROW_RIGHT:
+	    if (E.cx != E.screencols - 1)
+	    { E.cx++; }
+	    break;
+	  case ARROW_UP:
+	    if (E.cy != 0)
+	    { E.cy--; }
+	    break;
+	  case ARROW_DOWN:
+	    if (E.cy != E.screenrows - 1)
+	    { E.cy++; }
+	    break;
+	}
+}
+
+void ted_processKeypress(void)
+{
+	int c = ted_readKey();
+
+	switch (c)
+	{
+	  case CTRL_KEY('q'): exit(0);
+	    write(STDOUT_FILENO, "\x1b[2J", 4);
+	    write(STDOUT_FILENO, "\x1b[H", 3);
+	    exit(0);
+	    break;
+
+	  case HOME_KEY:
+	    E.cx = 0;
+	    break;
+	  case END_KEY:
+	    E.cx = E.screencols - 1;
+	    break;
+
+	  case PAGE_UP:
+	  case PAGE_DOWN:
+	    {
+	      int times = E.screenrows;
+	      while (times--)
+	      { ted_moveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN); }
+	    }
+	    break;
+
+	  case ARROW_UP:
+	  case ARROW_DOWN:
+	  case ARROW_LEFT:
+	  case ARROW_RIGHT:
+	    ted_moveCursor(c);
+	    break;
+
+	  default: break;
+	}
+}
+
+void ted_drawRows(struct abuf* ab)
+{
+	int y = 0;
+	for (; y < E.screenrows; y++)
+	{
+	  if (y == E.screenrows/3)
+	  {
+	    char welcome[80];
+	    int welcomelen = snprintf(welcome, sizeof(welcome),
+	      "Kilo editor -- version %s", KILO_VERSION);
+	    if (welcomelen > E.screencols)
+	    { welcomelen = E.screencols; }
+	    int padding = (E.screencols - welcomelen) / 2;
+	    if (padding)
+	    {
+	      ab_append(ab, "~", 1);
+	      padding--;
+	    }
+	    while(padding--){ ab_append(ab, " ", 1); }
+	    ab_append(ab, welcome, welcomelen);
+	  }
+	  else{ ab_append(ab, "~", 1); }
+
+	  ab_append(ab, "\x1b[K", 3);
+	  if (y < E.screenrows - 1)
+	  { ab_append(ab, "\r\n", 2); }
+	}
+}
+
+void ted_refreshScreen(void)
+{
+	struct abuf ab = ABUF_INIT;
+
+	ab_append(&ab, "\x1b[?25l", 6); // Hide cursor
+	//ab_append(&ab, "\x1b[2J", 4);
+	ab_append(&ab, "\x1b[H", 3);
+
+	ted_drawRows(&ab);
+
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy+1, E.cx+1);
+	ab_append(&ab, buf, strlen(buf));
+
+	ab_append(&ab, "\x1b[?25h", 6); // Show cursor
+
+	write(STDOUT_FILENO, ab.b, ab.len);
+	ab_free(&ab);
+}
+
 void disableRawMode(void)
 {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 	{ deathToMachine("tcsettattr"); }
 }
 
 void enableRawMode(void)
 {
 	//  Make copy of terminals original status
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 	{ deathToMachine("tcsettattr"); }
 	//  Restore terminal status at program exit
 	atexit(disableRawMode);
 
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 	raw.c_iflag &= ~BRKINT;
 	raw.c_iflag &= ~ICRNL; // Disable carriage return and new line
 	raw.c_iflag &= ~INPCK;
